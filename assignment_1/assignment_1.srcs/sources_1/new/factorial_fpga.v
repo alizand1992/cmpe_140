5a519c499e3193580ebab26f42077fb5c463e680
damienlevitt <damien.levitt@sjsu.edu>
2020-02-11 09:43:37 -0800

    added to the fpga file, cu_tb still not functional

--- assignment_1/assignment_1.srcs/sources_1/new/factorial_fpga.v
+++ assignment_1/assignment_1.srcs/sources_1/new/factorial_fpga.v
@@ -1,26 +1,28 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/11/2020 09:32:31 AM
-// Design Name: 
-// Module Name: factorial_fpga
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module factorial_fpga(
-
+    input clk100MHz, clk_btn, go,
+    input [4:0] n,
+    
+    output [3:0] cs,
+    output done, error,
+    output [7:0] LEDOUT,
+    output [3:0] LEDSEL    
+);
+    wire gt12, lt1;
+    wire clk, clk_5KHz, DONT_USE;
+ 
+    
+    clk_gen CLK (
+        .clk100MHz(clk100MHz), .clk_4sec(DONT_USE), .clk_5KHz(clk_5KHz)
+    );
+    
+    button_debouncer CLK_DB(
+        .clk(clk_5KHz), .button(clk_btn), .debounced_button(clk)
+    );
+    
+    CU_DP cudp (
+        .n(n), .clk(clk), .go(go),
+        .done(done), .error(error), .gt12(gt12), .lt1(lt1), .cs(cs)
     );
 endmodule
