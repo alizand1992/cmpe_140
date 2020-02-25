214d98da00dfeefb4c843af64dc8c1150d6395dc
Ali Zand <ali.zand@realpage.com>
2020-02-25 13:11:54 -0800

    part 3

--- assignment_3/assignment_3_part_3.asm
+++ assignment_3/assignment_3_part_3.asm
@@ -4,7 +4,7 @@ ori $s0, $0, 1  # f = 1
 sw $a0, 0($0)
 
 while:  slti  $t2, $a0, 2    	# if n < 2 -> 1
-	beq   $t2, $t3, end
+	bne   $t2, $0, end
 	mult  $s0, $a0		# f = f * n
 	mflo  $s0
 	addi  $a0, $a0, -1	# n = n - 1
