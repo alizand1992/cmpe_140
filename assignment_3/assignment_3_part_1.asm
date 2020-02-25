b1d87f5f7bcec70b545600ed9d567b68341ddee4
Ali Zand <ali.zand@realpage.com>
2020-02-25 13:07:03 -0800

    Stuff

--- assignment_3/assignment_3_part_1.asm
+++ assignment_3/assignment_3_part_1.asm
@@ -3,37 +3,38 @@ ori $a1, $0, 0x00A9	# b
 ori $s0, $0, 1974	# c
 
 ori $t1, $0, 0x0020
-ori $t2, $0, 0x0024
-ori $t3, $0, 0x002c
 
-mul $s1, $a0, $a0	# x = a * a
+mult $a0, $a0	# x = a * a
+mflo $s1
 sw  $s1, 0($t1)		# -> 0x20
-mul $s2, $s1, $a1	# y = x * b
-sw  $s2, 0($t2)		# -> 0x24
+mult $s1, $a1	# y = x * b
+mflo $s2
+sw  $s2, 4($t1)		# -> 0x24
+mfhi $t2
+sw  $t2, 8($t1)
 
 srl $s2, $s2, 16	# y = y >> 16
 
-j   calc
 
-	div $t4, $s2, $s0	#          y / c
+	div $s2, $s0	#          y / c
+	mflo $t4
 	add $t4, $s0, $t4	#      c +   ^
 	srl $s0, $t4, 1		# c =    ^        / 2
-	sw  $s0, 0($t3)	# -> 0x2C
+	sw  $s0, 12($t1)	# -> 0x2C
 
-if:
-	slti $t5, $s0, 1665 	# if (c < 1665) 1
+# c = (c + y / c) / 2
+calc:	div $s2, $s0	#          y / c
+	mflo $t4
+	add $t4, $s0, $t4	#      c +   ^
+	srl $s0, $t4, 1		# c =    ^        / 2
+
+if:	slti $t5, $s0, 1665 	# if (c < 1665) 1
 	beq  $t5, $0, while
 	j    end
 
-while:
-	j calc
 
-calc:	# c = (c + y / c) / 2
-	div $t4, $s2, $s0	#          y / c
-	add $t4, $s0, $t4	#      c +   ^
-	srl $s0, $t4, 1		# c =    ^        / 2
-	j   if
+while:	j calc
 
-end:
-	sll $s0, $s0, 8
-	sw  $s0, 0($t3)	# -> 0x2C
+
+end:	sll $s0, $s0, 8
+	sw  $s0, 12($t1)	# -> 0x2C
--- assignment_3/assignment_3_part_3.asm
+++ assignment_3/assignment_3_part_3.asm
@@ -1,16 +1,14 @@
 ori $a0, $0, 5	# n = 5
 ori $s0, $0, 1  # f = 1
-ori $t1, $0, 0
-ori $t3, $0, 1
 
-sw $a0, 0($t1)
+sw $a0, 0($0)
 
-while:
-	slti $t2, $a0, 2    	# if n < 2 -> 1
-	beq  $t2, $t3, end
-	mul  $s0, $s0, $a0	# f = f * n
-	addi $a0, $a0, -1	# n = n - 1
-	j    while
+while:  slti  $t2, $a0, 2    	# if n < 2 -> 1
+	beq   $t2, $t3, end
+	mult  $s0, $a0		# f = f * n
+	mflo  $s0
+	addi  $a0, $a0, -1	# n = n - 1
+	j     while
 
-end:
-	sw $s0, 16($t1)
+end:	mflo $s0
+	sw   $s0, 16($0)
